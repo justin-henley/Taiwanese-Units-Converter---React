1beea74ff2b1c96bc688f1427d719e6c8abfc416
justin-henley <justin.a.henley@gmail.com>
2022-02-13 14:32:11 +0800

    This seems better for styling, but neither way enforces numeric-only entry

--- src/NumEntry.js
+++ src/NumEntry.js
@@ -5,7 +5,8 @@ const NumEntry = ({ box, entries, numHandler }) => {
     <input
       id={"input" + box}
       className="numEntry"
-      type="number"
+      type="text"
+      inputMode="numeric"
       placeholder="Convert from"
       value={entries[box].value}
       onChange={(e) => numHandler(e.target.value, box)}
