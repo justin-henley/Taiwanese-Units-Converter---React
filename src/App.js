6f1c90a5042c993998642d3f6d9bfe01e3800b72
justin-henley <justin.a.henley@gmail.com>
2022-02-14 00:37:16 +0800

    todos added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
 
+// TODO fix the way unit changes effect across values
+// TODO fix floating point annooyances
+// TODO Add Mandarin for all units
+// TODO Add a language option to switch to Mandarin?
+// TODO Mobile-first CSS with mixins for viewport sizes
+// TODO Mobile can't hover so buttons don't animate/change color. Use a mixin to change behavior based on size to guess at mobile/tablet
+
 function App() {
   const appName = "Taiwanese Unit Conversion Calculator";
   return (
