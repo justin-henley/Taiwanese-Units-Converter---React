52d11e52b0eae4453e751324a74fd63547a7fc2f
justin-henley <justin.a.henley@gmail.com>
2022-02-13 14:18:06 +0800

    changed to number input

--- src/NumEntry.js
+++ src/NumEntry.js
@@ -4,7 +4,8 @@ const NumEntry = ({ box, entries, numHandler }) => {
   return (
     <input
       id={"input" + box}
-      type="text"
+      className="numEntry"
+      type="number"
       placeholder="Convert from"
       value={entries[box].value}
       onChange={(e) => numHandler(e.target.value, box)}
