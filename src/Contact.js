0e2cf544d0ab4f80328bfdf83a6ea4516901918a
justin-henley <justin.a.henley@gmail.com>
2022-02-15 16:23:44 +0800

    Moving css into styled components

--- src/Contact.js
+++ src/Contact.js
@@ -1,41 +1,85 @@
 import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
+import styled, { css } from "styled-components";
 
 const Contact = () => {
-  return (
-    <div className="Contact">
-      <p>Find me at:</p>
-      <div className="social">
-        <a
-          className="socialLink"
-          id="githubLink"
-          href="https://github.com/justin-henley"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub className="socialIcon" />
-        </a>
+  const icons = {
+    style: {
+      padding: "0.125em",
+      fontSize: "2em",
+    },
+    color: {
+      github: "#6cc644",
+      linkedin: "#2867b2",
+      twitter: "#1da1f2",
+    },
+  };
 
-        <a
-          className="socialLink"
-          id="linkedinLink"
+  return (
+    <ContactDiv>
+      <H1>Find me at:</H1>
+      <Links>
+        <LinkButton
+          href="https://github.com/justin-henley"
+          hoverColor={icons.color.github}
+        >
+          <FaGithub style={icons.style} />
+        </LinkButton>
+        <LinkButton
           href="https://www.linkedin.com/in/justin-henley/"
-          target="_blank"
-          rel="noopener noreferrer"
+          hoverColor={icons.color.linkedin}
         >
-          <FaLinkedin className="socialIcon" />
-        </a>
-        <a
-          className="socialLink"
-          id="twitterLink"
-          href="http://www.google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaTwitter className="socialIcon" />
-        </a>
-      </div>
-    </div>
+          <FaLinkedin style={icons.style} />
+        </LinkButton>
+        <LinkButton href="" hoverColor={icons.color.twitter}>
+          <FaTwitter style={icons.style} />
+        </LinkButton>
+      </Links>
+    </ContactDiv>
   );
 };
+
+const ContactDiv = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 24px;
+  background-color: darkslategray;
+  color: white;
+  border-radius: 5px;
+  margin: 0 0 0 1px;
+  border-radius: 5px 0 5px 0;
+`;
+
+const H1 = styled.h1`
+  font-size: 1em;
+  font-weight: normal;
+`;
+
+const Links = styled.div`
+  margin: 0px;
+`;
+
+const LinkButton = styled.a.attrs((props) => ({
+  target: "blank",
+  rel: "noopener noreferrer",
+  href: props.href,
+}))`
+  display: inline-block;
+  text-decoration: none;
+  color: darkslategray;
+  background-color: white;
+  border-radius: 10px 0px 10px 0px;
+  transition: all 0.2s ease-in-out;
+  font-size: 1em
+  padding: 0;
+  margin: 5px;
+
+  &:hover {
+    color: ${(props) => props.hoverColor};
+    transform: scale(1.2);
+  }
+`;
 
 export default Contact;
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,21 +1,77 @@
 import React from "react";
+import styled from "styled-components";
 
 const Portfolio = () => {
   const today = new Date();
+  const icons = {
+    style: {
+      padding: "0.25em",
+      fontSize: ".9em",
+    },
+    color: {
+      portfolio: "green",
+    },
+  };
 
   return (
-    <div className="Portfolio">
-      <p>Find this app and more at</p>
-      <a
+    <PortfolioDiv>
+      <H1>Find this app and more at</H1>
+      <LinkButton
         href="https://justin-henley.github.io/"
-        target="_blank"
-        rel="noopener noreferrer"
+        hoverColor={icons.color.portfolio}
+        style={icons.style}
       >
         justin-henley.github.io
-      </a>
-      <p id="copyright">&copy;{today.getFullYear()} Justin Henley</p>
-    </div>
+      </LinkButton>
+      <Copyright id="copyright">
+        &copy;{today.getFullYear()} Justin Henley
+      </Copyright>
+    </PortfolioDiv>
   );
 };
+
+const PortfolioDiv = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 24px;
+  background-color: darkslategray;
+  color: white;
+  border-radius: 5px;
+  margin: 0 1px 0 0;
+  border-radius: 0 5px 0 5px;
+`;
+
+const H1 = styled.h1`
+  font-size: 1em;
+  font-weight: normal;
+`;
+
+const LinkButton = styled.a.attrs((props) => ({
+  target: "blank",
+  rel: "noopener noreferrer",
+  href: props.href,
+}))`
+  display: inline-block;
+  text-decoration: none;
+  color: darkslategray;
+  background-color: white;
+  border-radius: 10px 0px 10px 0px;
+  transition: all 0.2s ease-in-out;
+  font-size: 1em
+  padding: 0;
+  margin: 5px;
+
+  &:hover {
+    color: ${(props) => props.hoverColor};
+    transform: scale(1.2);
+  }
+`;
+
+const Copyright = styled.p`
+  font-size: 0.75rem;
+`;
 
 export default Portfolio;
