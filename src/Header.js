fa2ae4f67819eb5aca0447230bf9dc26ff108a74
justin-henley <justin.a.henley@gmail.com>
2022-02-15 16:53:39 +0800

    moved header css into styled components

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,29 @@
 import React from "react";
+import styled from "styled-components";
 
 const Header = ({ appName }) => {
   return (
-    <header>
-      <p>{appName}</p>
-    </header>
+    <HeaderDiv>
+      <H1>{appName}</H1>
+    </HeaderDiv>
   );
 };
+
+const HeaderDiv = styled.header`
+  width: 100%;
+  height: 100px;
+  background-color: darkslategray;
+  color: white;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  border-radius: 5px 5px 0 0;
+`;
+
+const H1 = styled.h1`
+  text-align: center;
+  font-size: 1.5rem;
+  font-weight: normal;
+`;
 
 export default Header;
